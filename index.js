ea5c5acb45fb17818114fe930c235d4253566cd3
yishnu96 <yishnupramanik96@gmail.com>
2020-06-30 18:19:42 +0530

    Set up layout and Database

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.set(express.static('./assets'))
+app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/',require('./routes'))            //Sending to router files
+app.set('view engine', 'ejs');      //setting view engine
+app.set('views' , './views');
 
 //Server is UP
 app.listen(port,function(err){
     if(err){
-        console.log(`Error : ${err}`)
+        console.log(`Error in Server: ${err}`)
     }
     console.log(`Server running on PORT : ${port}`)
 })
