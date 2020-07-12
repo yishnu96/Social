4a09fafb18a9ea6a049b822fb130ccdf1f930331
yishnu96 <yishnupramanik96@gmail.com>
2020-07-13 01:34:31 +0530

    added flash massage

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware =require('node-sass-middleware');
-
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
 app.use(sassMiddleware({
     src: './assets/scss',
@@ -19,6 +20,7 @@ app.use(sassMiddleware({
     outputStyle: 'extended',
     prefix: '/css'
 }));
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -64,6 +66,8 @@ app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
 
+app.use(flash());
+app.use(customMware.setFlash);
 // use express router
 app.use('/', require('./routes'));
 
